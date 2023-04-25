cd7eb53a4a772fea25e6eb01bf3c2ce7002731b7
Risper <58802523+RisperGitau@users.noreply.github.com>
2023-04-25 22:35:26 +0300

    Add files via upload

---
+++ IMUcode.ino
@@ -0,0 +1,91 @@
+#include <Arduino_LSM6DSOX.h>
+
+#include "DHT.h"
+
+#define DHTPIN 12     
+#define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE ,12);
+
+float Ax, Ay, Az;
+float Gx, Gy, Gz;
+
+void setup() {
+  
+  Serial.begin(9600);
+  //Serial.begin(115200);
+  Serial.println(F("DHT11 example!"));
+  dht.begin();  
+
+   
+  while(!Serial);
+
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println("Hz");
+  Serial.println();
+
+  Serial.print("Gyroscope sample rate = ");  
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println("Hz");
+  Serial.println();
+
+}
+
+void loop() {
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+    
+    Serial.print(F("Humidity: "));
+    Serial.print(humidity);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(temperature);
+    Serial.println(F("°C "));
+
+  }
+  
+    if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(Ax, Ay, Az);
+
+    Serial.println("Accelerometer data: ");
+    Serial.print(Ax);
+    Serial.print('\t');
+    Serial.print(Ay);
+    Serial.print('\t');
+    Serial.println(Az);
+    Serial.println();
+  }
+
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(Gx, Gy, Gz);
+    
+    Serial.println("Gyroscope data: ");
+    Serial.print(Gx);
+    Serial.print('\t');
+    Serial.print(Gy);
+    Serial.print('\t');
+    Serial.println(Gz);
+    Serial.println();
+  }
+
+delay(500 );
+
+}
+
+
+
+
+
+
